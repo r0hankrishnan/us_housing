7c9e7e9875e5aabb068ab1ded9fd53978659becf
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-07-26 16:35:23 -0400

    Create useful-functions.R

---
+++ scripts/useful-functions.R
@@ -0,0 +1,20 @@
+
+#Take df, split based on parameters, return train and test set to global env
+train_test_split <- function(df, propTrain, propTest){
+  #Set seed for reproducibility
+  set.seed(100)
+  
+  stopifnot(is.double(propTrain)==T, is.double(propTest)==T, 
+            propTrain<1 & propTrain>0, propTest<1 & propTest>0)
+  
+  #Generate T/F vector of length nrow(df) to sample df
+  sample <- sample(c(TRUE,FALSE), nrow(df),  
+                   replace=TRUE, prob=c(propTrain,propTest))
+  
+  #Sample df with .70 T values for train & assign to global env
+  train  <<- df[sample, ] 
+  #Take rest as test & assign to global env
+  test  <<- df[!sample, ]
+
+}
+
