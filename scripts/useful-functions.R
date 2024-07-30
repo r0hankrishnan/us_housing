5a46a2f9ced4ebd6ac23fb0fb58aaf8e356903d3
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-07-30 10:16:38 -0400

    Update useful-functions.R
    Allow for naming of training and test sets

--- scripts/useful-functions.R
+++ scripts/useful-functions.R
@@ -4,18 +4,19 @@ train_test_split <- function(df, propTrain, propTest, trainName, testName){
   #Set seed for reproducibility
   set.seed(100)
   
+  #Check types and make sure proportion values are within 0 and 1
   stopifnot(is.double(propTrain)==T, is.double(propTest)==T, 
-            propTrain<1 & propTrain>0, propTest<1 & propTest>0)
-  
-  trainName <<- data.frame()
-  testName <<- data.frame()
+            propTrain<1 & propTrain>0, propTest<1 & propTest>0,
+            propTrain + propTest == 1, is.character(trainName), 
+            is.character(testName))
   
   #Generate T/F vector of length nrow(df) to sample df
   sample <- sample(c(TRUE,FALSE), nrow(df),  
                    replace=TRUE, prob=c(propTrain,propTest))
   
   #Sample df with .70 T values for train & assign to global env
-  train  <<- df[sample, ] 
+  assign(trainName, df[sample, ], envir = .GlobalEnv)
+  
   #Take rest as test & assign to global env
-  test  <<- df[!sample, ]
+  assign(testName, df[!sample, ], envir = .GlobalEnv)
 }
